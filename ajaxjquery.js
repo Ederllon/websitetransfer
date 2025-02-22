4c6f5af13f77b10bddc75200d5f596921eee8bb6
Ederllon <156702239+Ederllon@users.noreply.github.com>
2025-02-22 13:22:13 -0300

    Update ajaxjquery.js

--- ajaxjquery.js
+++ ajaxjquery.js
@@ -8,6 +8,17 @@ $(function () {
                     : 1 == modeType_dark
                     ? ((modeType_dark = !1), $("#buttonMode").attr("title", "Ativar DARK MODE nativo"))
                     : ($("#buttonMode").removeAttr("title"), console.log("Title removido!"));
+
+                
+
             });
     });
 });
+
+// let linkIcon = $('<link>').attr('rel','icon').attr('type','image/png').attr('href', 'favicon_light.ico');
+// $('head').append(linkIcon);
+// 0 == modeType_dark
+// ? ((modeType_dark = !0), $('link[rel="icon"]').attr('href', 'favicon_light.ico'))
+// : 1 == modeType_dark
+// ? ((modeType_dark = !1), $('link[rel="icon"]').attr('href', 'favicon_dark.ico'))
+// : ($('link[rel="icon"]').removeAttr("href"), console.log("Favicons removido!"));
